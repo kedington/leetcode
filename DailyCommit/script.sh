f89ceedf8d276b329b2f34e491da8d1ff5ba67c9
Kevin Edington <kedingto@ucsd.edu>
2018-08-27 22:14:40 -0700

    Bash fix

--- DailyCommit/script.sh
+++ DailyCommit/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILENAME=git status --short | cut -d " " -f 2 | grep -m 1 "" 
+FILENAME=$(git status --short | cut -d " " -f 2 | grep -m 1 "")
 git add $FILENAME
 git commit -m "Add " + $FILENAME
 git push origin master
