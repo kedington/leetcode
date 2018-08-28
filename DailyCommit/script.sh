33433f24347811279f5d740392f8f2b720db4678
Kevin Edington <kedingto@ucsd.edu>
2018-08-27 22:11:09 -0700

    Testing updated script

--- DailyCommit/script.sh
+++ DailyCommit/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git status --short | cut -d " " -f 2 | grep -m 1 "" | xargs git add
-git commit -m "Test commit"
+FILENAME=git status --short | cut -d " " -f 2 | grep -m 1 "" 
+git add $FILENAME
+git commit -m "Add " + $FILENAME
 git push origin master
 
