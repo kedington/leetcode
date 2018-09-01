d40206e6061eda7a290290924916f749410fcb87
kedington <kev4725@gmail.com>
2018-08-31 17:00:01 -0700

    Adding easy/maximumSubarray.py

---
+++ easy/maximumSubarray.py
@@ -0,0 +1,18 @@
+# Question : 53
+# Time : O(n)
+# Size : O(1)
+# Runtime: 50 ms 
+
+class Solution:
+    def maxSubArray(self, nums):
+        if not len(nums):
+            return None
+        greatest = nums[0]
+        current = greatest
+        for num in nums[1:]:
+            current += num
+            if num > current:
+                current = num 
+            if current > greatest:
+                greatest = current
+        return greatest
