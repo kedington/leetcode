506169e1bd6d8feaaed2f35fa06cf44c4d289174
kedington <kev4725@gmail.com>
2019-02-05 13:54:38 -0800

    Updated path

--- DailyCommit/script.sh
+++ DailyCommit/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /Users/snapIoTMini3/test/LeetCode 
+cd /Users/kevin/Personal/leet-code
 FILENAME=$(git status --short | cut -d " " -f 2 | grep -m 1 "")
 git add $FILENAME
 git commit -m "Adding ${FILENAME}"
