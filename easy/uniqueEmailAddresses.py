cc24cbca313ac439efd027658529761986f33f18
kedington <kev4725@gmail.com>
2019-02-06 16:29:07 -0800

    Unique Email Addresses

---
+++ easy/uniqueEmailAddresses.py
@@ -0,0 +1,15 @@
+# Question : 929 
+# Time : O(n)
+# Space : O(n)
+# Runtime : 48 ms
+
+class Solution:
+    def numUniqueEmails(self, emails: 'List[str]') -> 'int':
+        email_list = set()
+        for email in emails:
+            name, domain = email.split('@')[0:2]
+            name = name.replace(".", "")
+            name = name.split('+')[0]
+            email_list.add(name + domain)
+        return len(email_list)
+
