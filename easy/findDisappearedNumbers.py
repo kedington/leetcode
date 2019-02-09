7b7ed2274bbb60de8b20aa11d307a8a878724695
kedington <kev4725@gmail.com>
2019-02-08 17:42:48 -0800

    Adding findDisappearedNumbers

---
+++ easy/findDisappearedNumbers.py
@@ -0,0 +1,12 @@
+# Question : 448                                                                
+# Time : O(n)
+# Space : O(n)
+# Runtime : 140 ms
+
+class Solution:
+    def findDisappearedNumbers(self, nums: 'List[int]') -> 'List[int]':
+        foo = set(range(1,len(nums)+1))
+        for num in nums:
+            if num in foo:
+                foo.remove(num)
+        return list(foo)
