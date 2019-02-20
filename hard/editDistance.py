285f8e29822f10781360c1e8a2404720b9329e26
kedington <kev4725@gmail.com>
2019-02-20 13:14:33 -0800

    Update editDistance

--- hard/editDistance.py
+++ hard/editDistance.py
@@ -3,32 +3,22 @@
 # Space : O(NxM)
 # Runtime : 200 ms
 
-def edit_distance(str1, str2):
-	len_str1 = len(str1)
-	len_str2 = len(str2)
-	result_matrix = prepare_matrix(len_str1+1, len_str2+1)
-	for y in range(1,len_str2+1):
-		for x in range(1,len_str1+1):
-			if str1[x-1] == str2[y-1]:
-				result_matrix[y][x] = result_matrix[y-1][x-1]
-			else:
-				result_matrix[y][x] = 1 + min(result_matrix[y-1][x],
-									  		  result_matrix[y][x-1],
-									  		  result_matrix[y-1][x-1])
-	return result_matrix
+class Solution:
+    def minDistance(self, word1: 'str', word2: 'str') -> 'int':
+        len_str1 = len(word1)
+        len_str2 = len(word2)
 
-def prepare_matrix(width, height):
-	matrix = []
-	for y in range(height):
-		inner_matrix = []
-		for x in range(width):
-			if y == 0:
-				inner_matrix.append(x)
-			elif x == 0:
-				inner_matrix.append(y)
-			else:
-				inner_matrix.append(0)
-		matrix.append(inner_matrix)
-	return matrix
-				
-			
+        result_matrix = [[0 for x in range(len_str1+1)] for y in range(len_str2+1)]                  
+        for y in range(len_str2+1):
+            for x in range(len_str1+1):
+                if y == 0:
+                    result_matrix[y][x] = x 
+                elif x == 0:
+                    result_matrix[y][x] = y 
+                elif word1[x-1] == word2[y-1]:
+                    result_matrix[y][x] = result_matrix[y-1][x-1]
+                else:
+                    result_matrix[y][x] = 1 + min(result_matrix[y-1][x],
+                                                  result_matrix[y][x-1],
+                                                  result_matrix[y-1][x-1])
+        return result_matrix[len_str2][len_str1]
