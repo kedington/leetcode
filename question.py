5c479a9683e477b230d715120b36e8300024df21
kedington <kev4725@gmail.com>
2019-03-05 15:26:36 -0800

    Adding a question class to hold details of the questions

---
+++ question.py
@@ -0,0 +1,11 @@
+class Question:
+
+	def __init__(self, name, number, link, difficulty, file_name):
+		self.name = name
+		self.number = number
+		self.link = link
+		self.difficulty = difficulty
+		self.file_name = file_name
+
+
+
